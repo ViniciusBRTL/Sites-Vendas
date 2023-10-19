cd01fad0dfb976a619cb4293dc5873f7ac0486e6
Diogo-LP <140905190+Diogo-LP@users.noreply.github.com>
2023-10-19 15:51:50 -0300

    sadad

--- UrbanStreet/assets/js/produtos.js
+++ UrbanStreet/assets/js/produtos.js
@@ -1,51 +1,75 @@
 var camisetas = [
     {
         nome: "asdadw",
-        preco: "R$80,00",
+        preco: "80,00",
         img: "assets/img/camisas/MOD1.jpg",
         imgtras: "assets/img/camisas/MOD1TRAS.jpg",
         cor: "p"
     },  {
         nome: "asdadw",
-        preco: "R$80,00",
+        preco: "90,00",
         img: "assets/img/camisas/MOD2.jpg",
         imgtras: "assets/img/camisas/MOD2TRAS.jpg",
         cor: "p"
     }, {
         nome: "asdadw",
-        preco: "R$80,00",
+        preco: "90,00",
+        img: "assets/img/camisas/MOD2.jpg",
+        imgtras: "assets/img/camisas/MOD2TRAS.jpg",
+        cor: "p"
+    }, {
+        nome: "asdadw",
+        preco: "80,00",
         img: "assets/img/camisas/MOD6.jpg",
         imgtras: "assets/img/camisas/MOD6TRAS.jpg",
         cor: "p"
     }, {
         nome: "asdadw",
-        preco: "R$80,00",
+        preco: "80,00",
         img: "assets/img/camisas/MOD3.jpg",
         imgtras: "assets/img/camisas/MOD3TRAS.jpg",
         cor: "b"
     },{
         nome: "asdadw",
-        preco: "R$80,00",
+        preco: "80,00",
         img: "assets/img/camisas/MOD4.jpg",
         imgtras: "assets/img/camisas/MOD4TRAS.jpg",
         cor: "b"
     },{
         nome: "asdadw",
-        preco: "R$80,00",
+        preco: "80,00",
         img: "assets/img/camisas/MOD5.jpg",
         imgtras: "assets/img/camisas/MOD5TRAS.jpg",
         cor: "b"
     },{
         nome: "asdadw",
-        preco: "R$80,00",
+        preco: "80,00",
         img: "assets/img/camisas/MOD7.jpg",
         imgtras: "assets/img/camisas/MOD7TRAS.jpg",
         cor: "b"
     },{
         nome: "asdadw",
-        preco: "R$80,00",
+        preco: "80,00",
         img: "assets/img/camisas/MOD8.jpg",
         imgtras: "assets/img/camisas/MOD8TRAS.jpg",
         cor: "b"
     }
 ]
+var calcas = [
+    {
+
+    }
+]
+
+var moletons = [
+    {
+
+    }
+]
+
+var bones = [
+    {
+
+    }
+]
+
